c1a3ae4b5cb93d67f4768df4d052ad50459637ca
Abakrii <abdelrahmansalembakry@gmail.com>
2019-03-29 17:06:41 -0700

    Items Creation and Prisma Yoga Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,23 @@
 const Mutations = {
-    createDog(parent , args, ctx, info){
-        global.dogs = global.dogs || [];
-        //create a dog
-        const newDog = {name: args.name};
-        global.dogs.push(newDog);
-        return newDog;
-        console.log(args);
+    async createItem( parent , args, ctx, info){
+        //Todo  : Check if ther ar logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            },
+        }, info);
+        
+            return item;
     }
+    // createDog(parent , args, ctx, info){
+    //     global.dogs = global.dogs || [];
+    //     //create a dog
+    //     const newDog = {name: args.name};
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    //     console.log(args);
+    // }
 };
 
 module.exports = Mutations;
