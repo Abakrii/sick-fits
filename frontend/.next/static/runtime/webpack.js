7335fedb9936b3f25f423866d87e3f07bb9fd6ad
Abakrii <abdelrahmansalembakry@gmail.com>
2019-03-29 20:28:02 -0700

    Creating Items with Mutations

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f1b6cbdb404258e57300";
+/******/ 	var hotCurrentHash = "0b1e2d7a694c47282896";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import CreateItem from "../components/CreateItem";
+
 const sell = props =>(
     <div>
-        <p>sell Page!</p>
-     
+       <CreateItem/>
     </div>
     );
     
